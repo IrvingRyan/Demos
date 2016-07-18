cfb1c1948609bdafbb5a4a72f33e7deb152a0941
IrvingRyan <20463220632@qq.com>
2016-07-18 15:07:27 +0800

    some changes

---
+++ OkHttpDemo/app/src/main/java/com/irvingryan/okhttpdemo/http/HttpConfig.java
@@ -0,0 +1,9 @@
+package com.irvingryan.okhttpdemo.http;
+
+/**
+ * Created by wentao on 2016/7/18.
+ */
+public class HttpConfig {
+    public static final String PHONE_URL="http://apis.baidu.com/apistore/mobilenumber/mobilenumber?phone=";
+    public static final String DRIVER_TEST_URL="http://api2.juheapi.com/jztk/query";
+}
