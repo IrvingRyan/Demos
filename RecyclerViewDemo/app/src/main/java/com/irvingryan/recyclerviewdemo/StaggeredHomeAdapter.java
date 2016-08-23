c320976d70f261fdec587714fc1d40a7f0699c12
IrvingRyan <20463220632@qq.com>
2016-08-23 15:33:50 +0800

    完善一些信息

--- RecyclerViewDemo/app/src/main/java/com/irvingryan/recyclerviewdemo/StaggeredHomeAdapter.java
+++ RecyclerViewDemo/app/src/main/java/com/irvingryan/recyclerviewdemo/StaggeredHomeAdapter.java
@@ -99,6 +99,11 @@ class StaggeredHomeAdapter extends
 		return mDatas.size();
 	}
 
+	/**
+	 * 注意，这里更新数据集不是用adapter.notifyDataSetChanged()而是
+	 notifyItemInserted(position)与notifyItemRemoved(position)
+	 否则没有动画效果。
+     */
 	public void addData(int position)
 	{
 		mDatas.add(position, "Insert One");
