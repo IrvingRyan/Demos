f47cb7f0888b5c8abbd6cbb34f16b60684ca92b5
IrvingRyan <20463220632@qq.com>
2016-09-12 10:26:37 +0800

    RealmDemo added

---
+++ RealmDemo/app/src/main/java/com/irvingryan/realmdemo/MyApplication.java
@@ -0,0 +1,13 @@
+package com.irvingryan.realmdemo;
+
+import android.app.Application;
+
+/**
+ * Created by wentao on 2016/9/9.
+ */
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
