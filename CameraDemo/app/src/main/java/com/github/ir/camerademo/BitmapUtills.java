7b69aa54a8c12914ca8392652446229b25b14064
IrvingRyan <2046322063@qq.com>
2017-03-17 17:58:09 +0800

    处理摄像头的图片，加时间戳，在ImageView中显示

---
+++ CameraDemo/app/src/main/java/com/github/ir/camerademo/BitmapUtills.java
@@ -0,0 +1,31 @@
+package com.github.ir.camerademo;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by yanwentao on 2017/3/17.
+ */
+
+public class BitmapUtills {
+     static   SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy年 M月d日 hh:mm:ss");
+
+    public static Bitmap addTimeStamp(Bitmap bitmap){
+        Bitmap b = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        Date date = new Date();
+        String format = simpleDateFormat.format(date);
+        Canvas canvas = new Canvas(b);
+        canvas.drawBitmap(bitmap,0,0,new Paint());
+        Paint paint = new Paint();
+        paint.setColor(Color.RED);
+        paint.setTextSize(18);
+        canvas.drawText(format,20,20,paint);
+        return b;
+    }
+}
