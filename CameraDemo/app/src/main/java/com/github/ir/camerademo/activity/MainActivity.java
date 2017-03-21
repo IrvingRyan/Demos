19968fff9f46edd6b96f4246fb02c57f1d9d609b
IrvingRyan <2046322063@qq.com>
2017-03-21 23:08:32 +0800

    整理成一个可供查阅的Demo

--- CameraDemo/app/src/main/java/com/github/ir/camerademo/activity/MainActivity.java
+++ CameraDemo/app/src/main/java/com/github/ir/camerademo/activity/MainActivity.java
@@ -1,54 +1,57 @@
-package com.github.ir.camerademo;
+package com.github.ir.camerademo.activity;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Color;
 import android.graphics.ImageFormat;
 import android.graphics.Rect;
 import android.graphics.SurfaceTexture;
 import android.graphics.YuvImage;
 import android.hardware.Camera;
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.TextureView;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
+import android.view.View;
+
+import com.github.ir.camerademo.util.BitmapUtills;
+import com.github.ir.camerademo.R;
 
 import java.io.ByteArrayOutputStream;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements Camera.PreviewCallback,TextureView.SurfaceTextureListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener,Camera.PreviewCallback, TextureView.SurfaceTextureListener {
 
-    private String TAG="MainActivity";
+    private String TAG = "MainActivity";
 
     private Camera camera;
-    private int height=240;
-    private int width =320;
+    private int height = 240;
+    private int width = 320;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        LinearLayout root = (LinearLayout) findViewById(R.id.root);
-//        CameraPreview cameraPreview = new CameraPreview(this, Camera.open());
-//        cameraPreview.addPreviewCallback(this);
-//        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(200, 200);
-//        root.addView(cameraPreview,layoutParams);
-        GameDisplay gameDisplay = new GameDisplay(this, 200, 200);
-        root.addView(gameDisplay);
+        $(R.id.surface_view).setOnClickListener(this);
+        $(R.id.surface_texture).setOnClickListener(this);
+        $(R.id.texture_view).setOnClickListener(this);
 
     }
 
-    public void initCamera(SurfaceHolder holder){
+    public <T extends View> T $(@IdRes int id) {
+        return (T) findViewById(id);
+    }
+
+    public void initCamera(SurfaceHolder holder) {
         try {
             Camera.CameraInfo cameraInfo = new Camera.CameraInfo();
             int numberOfCameras = Camera.getNumberOfCameras();
-            Log.i("numberOfCameras","numberOfCameras = "+numberOfCameras);
-            for (int i=0;i<numberOfCameras;i++){
-                Camera.getCameraInfo(i,cameraInfo);
-                Log.i("cameraInfo"," facing = "+cameraInfo.facing+" orientation ="+cameraInfo.orientation+" canDisableShutterSound ="+cameraInfo.canDisableShutterSound);
+            Log.i("numberOfCameras", "numberOfCameras = " + numberOfCameras);
+            for (int i = 0; i < numberOfCameras; i++) {
+                Camera.getCameraInfo(i, cameraInfo);
+                Log.i("cameraInfo", " facing = " + cameraInfo.facing + " orientation =" + cameraInfo.orientation + " canDisableShutterSound =" + cameraInfo.canDisableShutterSound);
             }
 
             camera = Camera.open();
@@ -63,16 +66,16 @@ public class MainActivity extends AppCompatActivity implements Camera.PreviewCal
 
             //获取摄像头支持的数据格式
             List<Integer> supportedPreviewFormats = parameters.getSupportedPreviewFormats();
-            Log.i(""," supportedPreviewFormats  = "+supportedPreviewFormats.toString());
+            Log.i("", " supportedPreviewFormats  = " + supportedPreviewFormats.toString());
 
             List<Camera.Size> supportedPreviewSizes = parameters.getSupportedPreviewSizes();
-            for (Camera.Size size:supportedPreviewSizes){
-                Log.i("supportedPreviewSizes"," height:"+size.height +" width:"+size.width);
+            for (Camera.Size size : supportedPreviewSizes) {
+                Log.i("supportedPreviewSizes", " height:" + size.height + " width:" + size.width);
             }
 
             List<Camera.Size> supportedPictureSizes = parameters.getSupportedPictureSizes();
-            for (Camera.Size size:supportedPictureSizes){
-                Log.i("supportedPictureSizes"," height:"+size.height +" width:"+size.width);
+            for (Camera.Size size : supportedPictureSizes) {
+                Log.i("supportedPictureSizes", " height:" + size.height + " width:" + size.width);
 
             }
 
@@ -86,23 +89,24 @@ public class MainActivity extends AppCompatActivity implements Camera.PreviewCal
 
     }
 
-    public void destoryCamera(){
-        if (camera!=null){
+    public void destoryCamera() {
+        if (camera != null) {
             camera.setPreviewCallback(null);
             camera.stopPreview();
             camera.release();
-            camera=null;
+            camera = null;
         }
     }
+
     @Override
     public void onPreviewFrame(byte[] data, Camera camera) {
-        Log.i(TAG,"onPreviewFrame");
+        Log.i(TAG, "onPreviewFrame");
         Camera.Parameters parameters = camera.getParameters();
         Camera.Size previewSize = parameters.getPreviewSize();
         //以下操作将byte数据转换为bitmap 并显示
-        YuvImage image=new YuvImage(data,ImageFormat.NV21,previewSize.width,previewSize.height,null);
-        ByteArrayOutputStream stream=new ByteArrayOutputStream();
-        image.compressToJpeg(new Rect(0,0,previewSize.width,previewSize.height),100,stream);
+        YuvImage image = new YuvImage(data, ImageFormat.NV21, previewSize.width, previewSize.height, null);
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        image.compressToJpeg(new Rect(0, 0, previewSize.width, previewSize.height), 100, stream);
 
         Bitmap bitmap = BitmapFactory.decodeByteArray(stream.toByteArray(), 0, stream.size());
 
@@ -129,5 +133,20 @@ public class MainActivity extends AppCompatActivity implements Camera.PreviewCal
     @Override
     public void onSurfaceTextureUpdated(SurfaceTexture surface) {
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.surface_texture:
+                startActivity(new Intent(this,SurfaceTextureActivity.class));
+                break;
+            case R.id.texture_view:
+                startActivity(new Intent(this,TextureViewActivity.class));
+                break;
+            case R.id.surface_view:
+                startActivity(new Intent(this,SurfaceViewActivity.class));
+                break;
+        }
     }
 }
